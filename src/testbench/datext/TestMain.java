3fd456d4296c728ce9ef705addc7e336bd6d86e7
Christopher Hall <cybergnome@SpellBook>
2013-08-11 07:29:56 -0400

    Netbeans Project initialization

---
+++ src/testbench/datext/TestMain.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testbench.datext;
+
+/**
+ *
+ * @author cybergnome
+ */
+public class TestMain {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code test logic here
+	}
+}
