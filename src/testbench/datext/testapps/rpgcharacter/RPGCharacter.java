76493b1f166fe8689b96cc85337facea983c376c
cybergnome <cybergnome@SpellBook>
2013-11-25 11:24:38 -0500

    Started making an app to test DaText with real-world application

---
+++ src/testbench/datext/testapps/rpgcharacter/RPGCharacter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testbench.datext.testapps.rpgcharacter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author cybergnome
+ */
+public class RPGCharacter {
+
+	static final String DATA_DIRNAME = ".RPGCharacter";
+	static final String DATA_FILENAME = "data.dtx";
+	public static Path dataDir = Paths.get(DATA_DIRNAME); // working dir
+	
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			
+			if(System.getProperty("os.name").toLowerCase(Locale.US).contains("windows")){
+				// is windows
+				dataDir = Paths.get(System.getProperty("user.home"),"AppData","Roaming",DATA_DIRNAME);
+			} else {
+				// is not windows
+				dataDir = Paths.get(System.getProperty("user.home"),DATA_DIRNAME);
+			}
+			System.out.println("Initializing data directory:\n\t"+dataDir.toAbsolutePath().toString());
+			Files.createDirectories(dataDir);
+			
+			
+			// TODO code application logic here
+			
+		} catch (Exception ex) {
+			Logger.getLogger(RPGCharacter.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
